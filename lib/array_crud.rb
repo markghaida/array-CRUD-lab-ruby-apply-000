284465b02bf30d48ff0e9e244107e9ba43a68643
Mark G <markghaida@gmail.com>
2020-10-01 23:47:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ def remove_element_from_start_of_array(array)
  array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index(array, 2)
   array = ["wow",2,"am","arrays!"]
   puts array[index_number]
 end
