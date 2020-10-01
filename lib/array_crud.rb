afbceae39267ab748f931ccb0fcbd7b8937f3b70
Mark G <markghaida@gmail.com>
2020-10-01 23:38:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow",2,"am","arrays!"]
+  array = ["wow","am","am","arrays!"]
   puts array[2]
 end
 
