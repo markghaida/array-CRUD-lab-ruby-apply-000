eee0f29d91dbc816557042ad8b63fa8f0f19857d
Mark G <markghaida@gmail.com>
2020-10-01 23:49:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,12 +28,12 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow",2,"am","arrays!"]
-  puts array[2]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
   array = ["wow",2,"am","arrays!"]
-  puts array[index]
+  puts array[]
 end
 
 def retrieve_last_element_from_array(array)
