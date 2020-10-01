94aaf037f40394b51209e8d1b8337baf8287bd6e
Mark G <markghaida@gmail.com>
2020-10-01 23:32:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,21 +17,22 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  array = [1,2,3,4]
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = [1,2,3,4]
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = [1,2,3,4]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = [1,2,3,4]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = [1,2,3,4]
 end
