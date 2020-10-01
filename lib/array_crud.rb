9685932e108d837d57a0f67a79cf894dac05c11b
Mark G <markghaida@gmail.com>
2020-10-01 23:33:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,11 +23,12 @@ end
 
 def remove_element_from_start_of_array(array)
   array = [1,2,3,4]
-  array.
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
   array = [1,2,3,4]
+  
 end
 
 def retrieve_first_element_from_array(array)
